f348512770142a514b1ba15579d0bd496e2f6f27
Iftekher Aziz <azizchy02@gmail.com>
2023-02-10 01:55:33 +0600

    Commit

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+console.log(document.body);
+
+ const studnet = {
+            name: 'John',
+            age: 20,
+            study: function(time){
+                console.log(time, 'Studying');
+            }
+        };
+        console.log('Adding a new element');
+
+        const liCollections = document.getElementsByTagName('li');
+        console.log(liCollections);
+        for (const li of liCollections) {
+            li.style.color = 'green';
+            console.log(li);
+        }
+
---
+++ js/script.js
@@ -0,0 +1,83 @@
+// getElementById() – select an element by id
+// getElementsByTagName() – select elements by tag name
+// getElementsByClassName() – select elements by class name
+// querySelector() – select the first element that matches a CSS selector
+// querySelectorAll() – select all elements that match a CSS selector
+// getAttribute() – get the value of an attribute
+// setAttribute() – set the value of an attribute
+// removeAttribute() – remove an attribute
+// hasAttribute() – check if an attribute exists
+// innerHTML – get or set the HTML content
+// textContent – get or set the text content
+// style – get or set the style
+// addEventListener() – attach an event handler
+// appendChild() – add an element to the end of the list of children
+// insertBefore() – add an element before another element
+// removeChild() – remove an element from the list of children
+// replaceChild() – replace an element with another element
+// removeAll() – remove all
+// createElement() – create an element
+// createTextNode() – create a text node
+// cloneNode() – clone an element
+// classList – get or set the class name
+// parentElement – get the parent element
+// childNodes – get the list of children
+// firstChild – get the first child
+// lastChild – get the last child
+// nextSibling – get the next sibling
+// previousSibling – get the previous sibling
+// nodeName – get the node name
+// nodeType – get the node type
+// nodeValue – get or set the node value
+// append() – add an element to the end of the list of children
+// prepend() – add an element to the beginning of the list of children
+// before() – add an element before another element
+// after() – add an element after another element
+// remove() – remove an element
+// replaceWith() – replace an element with another element
+// clone() – clone an element
+// matches() – check if an element matches a CSS selector
+// closest() – get the closest ancestor that matches a CSS selector
+// add() – add a class
+// remove() – remove a class
+// toggle() – toggle a class
+// contains() – check if an element contains a class
+// forEach() – loop through a NodeList
+// map() – create a new array from a NodeList
+// filter() – create a new array from a NodeList, containing only elements that pass a test
+// reduce() – reduce the NodeList to a single value
+// every() – check if all elements in a NodeList pass a test
+// some() – check if some elements in a NodeList pass a test
+// find() – get the first element in a NodeList that passes a test
+// findIndex() – get the index of the first element in a NodeList that passes a test
+// keys() – get the keys of a NodeList
+// values() – get the values of a NodeList
+// entries() – get the key/value pairs of a NodeList
+// includes() – check if a NodeList contains an element
+// indexOf() – get the index of an element in a NodeList
+// join() – join the elements of a NodeList into a string
+// slice() – select a part of a NodeList, and return the new NodeList
+// sort() – sort the elements of a NodeList
+// reverse() – reverse the order of the elements in a NodeList
+// concat() – merge two or more NodeLists, and return a new NodeList
+// push() – add new elements to the end of a NodeList, and return the new length
+// pop() – remove the last element from a NodeList, and return that element
+// shift() – remove the first element from a NodeList, and return that element
+// unshift() – add new elements to the beginning of a NodeList, and return the new length
+// splice() – add/remove elements from a NodeList
+// toString() – convert a NodeList to a string
+// length – get the number of elements in a NodeList
+// item() – get an element by index number
+// namedItem() – get an element by name
+// length – get the number of elements in a NamedNodeMap
+// item() – get an element by index number
+// getNamedItem() – get an element by name
+// setNamedItem() – add an element
+// removeNamedItem() – remove an element
+// getNamedItemNS() – get an element by namespace and name
+// setNamedItemNS() – add an element
+// removeNamedItemNS() – remove an element
+// length – get the number of elements in a HTMLCollection
+// item() – get an element by index number
+// namedItem() – get an element by name
+// value() – get an element by
